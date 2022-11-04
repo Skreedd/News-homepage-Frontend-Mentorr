f1fce93a2543d527b5d0fa2f992b098ec3703bc7
Martin <107982777+Skreedd@users.noreply.github.com>
2022-11-04 22:38:52 +0000

    updated

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const hamburger = document.querySelector(".checkbtn");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+
+// document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+    
+// }));
+
+console.log("Hello world")
