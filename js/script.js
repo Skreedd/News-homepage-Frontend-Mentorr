86dbc6cf3cac1f05b21033027ac5cfd1b1f73e9d
Martin <107982777+Skreedd@users.noreply.github.com>
2022-11-06 22:15:34 +0000

    Updating Code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 const Hamburger = document.querySelector('#btnHamburger');
 const openMenu = document.querySelector('.has-fade');
 const body = document.querySelector('body');
-const menu = document.querySelector('.header__menu')
+const menu = document.querySelector('.header__menu');
+
 
 Hamburger.addEventListener('click', () => {
         console.log('click hamburger');
 
         if (Hamburger.classList.contains('open')) {
             Hamburger.classList.remove('open');
-            openMenu.classList.add('hide')
-            body.classList.remove('noscroll')
-            menu.classList.remove('header__menu')
+            body.classList.remove('noscroll');
+            menu.classList.remove('header__menu');
+            openMenu.classList.add('hide');
+            
         }
         else {
+            openMenu.classList.remove('hide');
             Hamburger.classList.add('open');
-            openMenu.classList.remove('hide')
-            body.classList.add('noscroll')
-            menu.classList.add('header__menu')
+            body.classList.add('noscroll');
+            menu.classList.add('header__menu');
         }
     });
